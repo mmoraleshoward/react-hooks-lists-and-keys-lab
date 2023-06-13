b797b79003d2096a68cec01cf6f11703b6ca7701
Morgan Morales-Howard <mmoraleshoward@gmail.com>
2023-06-12 22:53:54 -0500

    completion of lists and keys lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+            <a href = "#home" id = {links.home}>home</a>
+            <a href = "#about" id = {links.about}>about</a>
+            <a href = "#projects" id = {links.projects}>projects</a>
+        </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,15 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projectStuff = projects.map((projObj) => {
+    return (<ProjectItem key = {projObj.id} name = {projObj.name} about = {projObj.about} technologies={projObj.technologies} />)
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectStuff}</div>
     </div>
   );
 }
